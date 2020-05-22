419dd9232615009e9370a2b40f99b20ec125577a
bsrsuresh <Sureshb.ryhill@gmail.com>
2020-05-22 11:15:46 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+from Djangoapp import settings
+from django.core.mail import send_mail
+
+
+def mail(request):
+   subject = "Greetings"
+   msg = "welcome to python django mail to sent...."
+   to = "bsrtpt08@gmail.com"
+   res = send_mail(subject, msg, settings.EMAIL_HOST_USER, [to])
+   if (res == 1):
+      msg = "Mail Sent Successfuly"
+   else:
+      msg = "Mail could not sent"
+   return HttpResponse(msg)
